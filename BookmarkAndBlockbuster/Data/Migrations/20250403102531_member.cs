ec9b90ee290be18ab57cb6a78e02dd8577cf8d46
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-03 06:54:40 -0400

    Add the Model Member

---
+++ BookmarkAndBlockbuster/Data/Migrations/20250403102531_member.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookmarkAndBlockbuster.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class member : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemberName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    MemberPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberEmail = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
---
+++ BookmarkAndBlockbuster/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookmarkAndBlockbuster.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        public required string MemberName { get; set; }
+
+        public DateOnly? BirthDate { get; set; }
+
+        public required string MemberPhone { get; set; }
+
+        public string? MemberEmail { get; set; }
+    }
+}

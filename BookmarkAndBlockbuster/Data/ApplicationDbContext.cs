0af3748f812c20788b2b2cdf0db171da265a3d0d
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-03 11:21:32 -0400

    Add the model Bookxmovie to the database

--- BookmarkAndBlockbuster/Data/ApplicationDbContext.cs
+++ BookmarkAndBlockbuster/Data/ApplicationDbContext.cs
@@ -21,6 +21,8 @@ namespace BookmarkAndBlockbuster.Data
 
         public DbSet<MoviesLog> MoviesLogs { get; set; }
 
+        public DbSet<Bookxmovie> Booksxmovies { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {

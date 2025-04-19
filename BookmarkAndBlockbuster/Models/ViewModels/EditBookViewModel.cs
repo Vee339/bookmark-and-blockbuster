190288c2c1aa4497e3fbde444e4aeee1ccebc646
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-18 23:16:58 -0400

    Build the web pages for books

---
+++ BookmarkAndBlockbuster/Models/ViewModels/EditBookViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BookmarkAndBlockbuster.Models.ViewModels
+{
+    public class EditBookViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Author> Authors { get; set; }
+    }
+}
